9b30e06a1ad041ae00658de7f505b90774db85a8
mayagold <maya.gold.1991@gmail.com>
2017-08-09 09:59:40 -0700

    require bcrypt in server.js file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const bcrypt = require('bcrypt'),
 
 
 
