671532f6985933a47c6beee92242bb3d59f3d479
mayagold <maya.gold.1991@gmail.com>
2017-09-03 20:42:11 -0700

    styling posts index page with sticky navbar

--- public/app.js
+++ public/app.js
@@ -23,4 +23,20 @@ $( ()=>{
   }
   modalToggles.showLoginModal();
   modalToggles.showRegisterModal();
+  var $navbar = $(".banner-nav"),
+      y_pos = $navbar.offset().top,
+      height = $navbar.height();
+  $(document).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > y_pos + height) {
+      $navbar.fadeIn();
+      $navbar.addClass("navbar-fixed").animate({
+        top: 0
+      });
+    } else if (scrollTop <= y_pos) {
+      $navbar.removeClass("navbar-fixed").clearQueue().animate({
+        top: "-48px"
+      }, 0);
+    }
+  });
 })
